a97f89214f9932e3b3345e69157ab661ba72c8bc
yangtian <yangtian@tsinghua.org.cn>
2017-06-12 13:57:23 +0800

    simplify

--- find_k.py
+++ find_k.py
@@ -20,16 +20,12 @@ def find_k_use_math(integers, start):
             - sum_of_integers_exclude_k(integers, start))
 
 
-def find_who_was_reversed_twice(integers, offset):
-    for i in xrange(1, len(integers)):
-        if integers[i] > 0:
-            return i + offset
-    print "Not found k. Return -1."
-    return -1
-
-
 def is_pointing_inside_list(integers, pointer):
     return (0 < pointer and pointer < len(integers))
+
+
+def is_positive_number(integers, pointer):
+    return (integers[pointer] > 0)
 
 
 def reverse_pointed_value(integers, pointer):
@@ -40,8 +36,8 @@ def find_k_use_loop(integers, start):
     offset = start - 1
     for i in xrange(0, len(integers)):
         pointer = abs(integers[i]) - offset
-        if is_pointing_inside_list(integers, pointer):
+        if (is_pointing_inside_list(integers, pointer) and
+            is_positive_number(integers, pointer)):
             reverse_pointed_value(integers, pointer)
         else:
             return pointer + offset
-    return find_who_was_reversed_twice(integers, offset)
